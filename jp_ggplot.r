161fafe9db4412b39df0377eb4f56ee193c61ab0
procyk01 <procyk01@gmail.com>
2014-02-15 14:26:33 -0500

    rev1

--- jp_ggplot.r
+++ jp_ggplot.r
@@ -8,3 +8,8 @@ dsmall <- diamonds[sample(nrow(diamonds),100),]
 head(dsmall)
 
 qplot(carat,price, data=diamonds)
+qplot(log(carat), log(price), data=diamonds)
+qplot(carat, x*y*z, data = diamonds)
+qplot(carat, price, data=dsmall,color=color)
+qplot(carat, price, data=dsmall,shape=cut,color=color)
+
