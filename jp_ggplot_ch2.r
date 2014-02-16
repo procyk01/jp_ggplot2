daedb17253dbf436ff1aace66605ce641797a256
procyk01 <procyk01@gmail.com>
2014-02-15 21:27:05 -0500

    end ch2

--- jp_ggplot_ch2.r
+++ jp_ggplot_ch2.r
@@ -2,3 +2,8 @@
 
 library("ggplot2")
 head(mpg)
+
+qplot(displ, hwy, data=mpg, color=factor(cyl))
+qplot(displ, hwy, data=mpg) + geom_smooth()
+qplot(displ, hwy, data=mpg, facets=.~year) + geom_smooth()
+
